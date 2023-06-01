7e55e5c2454fbd68b9e7bdd336f78fcc118eeb13
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-06-01 17:12:05 +0530

    adding JWT token to client

--- src/routes/userLoginReg.ts
+++ src/routes/userLoginReg.ts
@@ -1,9 +1,20 @@
 import express, { Request, Response } from "express";
 import { LoginUser, RegisterUser } from "../services/credentialsUser.ts";
 import { userLogin, userRegProps } from "../services/IUser.ts";
+import { config } from "dotenv";
+import jwt from "jsonwebtoken";
+
+config();
+
+function generateAccessToken(username: string) {
+  return jwt.sign({ email: username }, process.env["JWT_TOKEN"]!, {
+    expiresIn: "1h",
+  });
+}
 
 const router = express.Router();
 router.use(express.json());
+
 router.post("/login", async (_req: Request, res: Response) => {
   const userDetails: userLogin = {
     email: _req.body.userDetails.email,
@@ -15,8 +26,11 @@ router.post("/login", async (_req: Request, res: Response) => {
       message: "User Not Found",
     });
   } else if (result === "login successful") {
+    const token = generateAccessToken(_req.body.userDetails.email);
     res.status(200).json({
       message: "Login Successful",
+      token: token,
+      payload: { email: _req.body.userDetails.email },
     });
   } else if (result === "wrong password")
     res.status(401).json({
