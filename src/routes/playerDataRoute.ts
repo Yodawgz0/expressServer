1223bf2e2d79f8cc61f1f8383d5a3538f6ce0a3f
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-06-02 15:42:55 +0530

    adding middleware for token verification

--- src/routes/playerDataRoute.ts
+++ src/routes/playerDataRoute.ts
@@ -6,34 +6,46 @@ import {
   getAllPlayerRecord,
 } from "../services/playerdata.ts";
 import { ObjectId } from "bson";
+import { AccessTokenVerify } from "../services/tokenProcess.ts";
 
 const playerData = express.Router();
 playerData.use(express.json());
 
-playerData.post("/playerData", async (_req: Request, res: Response) => {
-  AddPlayerData(_req.body.userDetails)
-    .then((response) => {
-      console.log(response);
-      res.status(201).json({ message: "Player Data is created successfully" });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ message: "Something Went Wrong" });
-    });
-});
+playerData.post(
+  "/playerData",
+  AccessTokenVerify,
+  async (_req: Request, res: Response) => {
+    AddPlayerData(_req.body.userDetails)
+      .then((_response) => {
+        res
+          .status(201)
+          .json({ message: "Player Data is created successfully" });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ message: "Something Went Wrong" });
+      });
+  }
+);
 
-playerData.get("/AllPlayerData", async (_req: Request, res: Response) => {
-  await getAllPlayerRecord()
-    .then((allPlayerData) => {
-      res.status(200).json({ data: allPlayerData });
-    })
-    .catch(() => {
-      res.status(500).json({ error: "Internal Server Error" });
-    });
-});
+playerData.get(
+  "/AllPlayerData",
+  AccessTokenVerify,
+  async (_req: Request, res: Response) => {
+    console.log(1, _req.cookies);
+    await getAllPlayerRecord()
+      .then((allPlayerData) => {
+        res.status(200).json({ data: allPlayerData });
+      })
+      .catch(() => {
+        res.status(500).json({ error: "Internal Server Error" });
+      });
+  }
+);
 
 playerData.delete(
   "/deletePlayerData/:cd&:pn&:id",
+  AccessTokenVerify,
   async (_req: Request, res: Response) => {
     if (_req.params["cd"] && _req.params["pn"] && _req.params["id"]) {
       await deletePlayer(new ObjectId(_req.params["id"]))
@@ -51,18 +63,27 @@ playerData.delete(
   }
 );
 
-playerData.patch("/editPlayer/:id", async (_req: Request, res: Response) => {
-  if (_req.params["id"]) {
-    await editOnePlayer(new ObjectId(_req.params["id"]), _req.body.userDetails)
-      .then(() => {
-        res.status(200).json({ message: "Player Data Modified Successfully" });
-      })
-      .catch(() => {
-        res.status(304).json({ error: "Internal Server Error" });
-      });
-  } else {
-    res.status(400).json({ error: "Not Enough Information Recvd" });
+playerData.patch(
+  "/editPlayer/:id",
+  AccessTokenVerify,
+  async (_req: Request, res: Response) => {
+    if (_req.params["id"]) {
+      await editOnePlayer(
+        new ObjectId(_req.params["id"]),
+        _req.body.userDetails
+      )
+        .then(() => {
+          res
+            .status(200)
+            .json({ message: "Player Data Modified Successfully" });
+        })
+        .catch(() => {
+          res.status(304).json({ error: "Internal Server Error" });
+        });
+    } else {
+      res.status(400).json({ error: "Not Enough Information Recvd" });
+    }
   }
-});
+);
 
 export { playerData };
