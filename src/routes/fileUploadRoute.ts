f9913b4dbbf83a5893b2956a07e81e1229982c01
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-06-15 22:36:56 +0530

    addiing multer

--- src/routes/fileUploadRoute.ts
+++ src/routes/fileUploadRoute.ts
@@ -1,12 +1,16 @@
 import express, { Request, Response } from "express";
 import { uploadFileHandler } from "../middlewares/fileupload.ts";
+// import multer from "multer"
 const uploadFile = express.Router();
 uploadFile.use(express.json());
 
-uploadFile.get(
+// const upload = multer({ dest: 'uploads/' });
+
+uploadFile.post(
   "/uploadfile",
 
   async (_req: Request, res: Response) => {
+    console.log(_req);
     await uploadFileHandler()
       .then((data) => res.status(200).json({ message: data }))
       .catch((err) => res.status(500).json({ message: err }));
