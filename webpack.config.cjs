8e1de646598bb7736649498c3b9787cd662c3d88
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-08-05 11:02:53 -0400

    adding webpack config

---
+++ webpack.config.cjs
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./index.ts",
+  output: {
+    path: path.join(__dirname, "dist"),
+    publicPath: "/",
+    filename: "final.js",
+  },
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+            experimentalWatchApi: true,
+            onlyCompileBundledFiles: true,
+          },
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
