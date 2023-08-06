c5793b6ac3c71120c307149fe4d443edcf320296
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-08-06 00:37:22 -0400

    updating with docker files and path

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "production",
-  entry: "./index.ts",
+  entry: "./api/index.ts",
   output: {
     path: path.join(__dirname, "dist"),
     publicPath: "/",
