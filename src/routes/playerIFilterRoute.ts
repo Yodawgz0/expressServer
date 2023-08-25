dcdd0800db7052843b6d031a45a78cb1f41d0f0a
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-08-24 22:46:06 -0400

    adding params to the shotmade palyername

--- src/routes/playerIFilterRoute.ts
+++ src/routes/playerIFilterRoute.ts
@@ -1,23 +1,28 @@
 import express, { Request, Response } from "express";
 import {
   fetchDistinctPlayersName,
-  fetchPlayerByName,
+  fetchPlayerByFilter,
 } from "../services/filterPlayerData.ts";
 import { AccessTokenVerify } from "../middlewares/tokenProcess.ts";
 
 const filterPlayer = express.Router();
 
 filterPlayer.get(
-  "/playerDataFilter/filter:playername",
-  AccessTokenVerify,
+  "/playerDataFilter/PLAYERNAME:playername?&SHOTMADE:shotmade?",
   async (_req: Request, res: Response) => {
-    if (_req.params["playername"]?.substring(1)) {
-      const namedPlayers = await fetchPlayerByName(
-        _req.params["playername"]?.substring(1)
+    if (
+      _req.params["playername"]?.substring(1) ||
+      _req.params["shotmade"]?.substring(1)
+    ) {
+      const namedPlayers = await fetchPlayerByFilter(
+        _req.params["playername"]?.substring(1) || "",
+        _req.params["shotmade"]?.substring(1) || ""
       );
       res.send({ data: namedPlayers }).status(200);
     } else {
-      res.send({ message: "Please Include the player name" }).sendStatus(400);
+      res
+        .send({ message: "ERROR: Filter shotmade or filtername missing" })
+        .sendStatus(400);
     }
   }
 );
