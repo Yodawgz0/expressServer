a8a0a485387ac47da41ad0227abb2edd62809b2e
Ashley <49788162+Yodawgz0@users.noreply.github.com>
2023-09-16 10:30:27 -0400

    removing comments

--- src/services/credentialsUser.ts
+++ src/services/credentialsUser.ts
@@ -2,11 +2,9 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 import { userLogin, userRegProps } from "../models/IUser.ts";
 import { config } from "dotenv";
 config();
-// Replace the placeholder with your Atlas connection string
 const uri: string = process.env["DB_URI"]!;
 const dbName: string = process.env["DB_NAME"]!;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
